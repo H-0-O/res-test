caf0b3a617ec8cba53f2465f23f70fed8b2fbe88
hossein salehi <ho.off.acc@gmail.com>
2025-08-31 13:09:21 +0330

    feat(seeder): write seeder to seed room type and its relations

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Inventory;
+use App\Models\Room;
+use App\Models\RoomType;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +16,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
+            'name' => 'Hossein Salehi',
             'email' => 'test@example.com',
         ]);
+
+        RoomType::factory()->count(3)->afterCreating(function(RoomType $roomType) {
+            $count = fake()->unique()->numberBetween(3 , 12);
+            $roomType->rooms()->createMany(
+                Room::factory()->count($count)->make()->toArray()
+            );
+            $roomType->inventory()->createMany(
+                Inventory::factory()->count(3)->make([
+                    'total_rooms' => $count,
+                    'available_rooms' => $count
+                ])->toArray()
+            );
+        })->create();
+        
     }
 }
