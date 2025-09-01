4bfa11a314c8d0be5753094be1df00cf35bb5dd9
hossein salehi <ho.off.acc@gmail.com>
2025-09-01 08:42:49 +0330

    fix(reservation-table): add  some indexes and request_count column

--- backend/database/migrations/2025_08_31_053627_create_reservations_table.php
+++ backend/database/migrations/2025_08_31_053627_create_reservations_table.php
@@ -16,9 +16,13 @@ return new class extends Migration
             $table->ulid('id')->primary();
             $table->foreignUlid('user_id')->constrained('users' , 'id');
             $table->foreignUlid('room_type_id')->constrained('room_types' , 'id');
-            $table->enum('status' , ReservationStatusEnum::cases());
-            $table->dateTime('expire_time');
+            $table->enum('status' , ReservationStatusEnum::cases())->index();
+            $table->date('date_to_reserve')->index();
+            $table->dateTime('expiration_time');
+            $table->integer('requested_count')->default(1);
             $table->timestamps();
+
+            $table->index(['status' , 'expiration_time']);
         });
     }
 
