0530c916ab92aebc640cc074fa55be8b4b5273e5
hossein salehi <ho.off.acc@gmail.com>
2025-09-01 08:49:45 +0330

    fix(room type): change inventory relation name to inventories

--- backend/app/Models/RoomType.php
+++ backend/app/Models/RoomType.php
@@ -5,11 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class RoomType extends Model
 {
     /** @use HasFactory<\Database\Factories\RoomTypeFactory> */
-    use HasFactory , HasUlids;
+    use HasFactory, HasUlids;
 
     protected $table = "room_types";
 
@@ -23,12 +24,13 @@ class RoomType extends Model
         'bed_count',
     ];
 
-    public function rooms(){
-        return $this->hasMany(Room::class , 'room_type_id' , 'id');
+    public function rooms(): HasMany
+    {
+        return $this->hasMany(Room::class, 'room_type_id', 'id');
     }
 
-    public function inventory(){
+    public function inventories(): HasMany
+    {
         return $this->hasMany(Inventory::class);
     }
-
 }
