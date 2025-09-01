64ae1cb5814f87505a47f3efdaff12d6c55219e8
hossein salehi <ho.off.acc@gmail.com>
2025-09-01 09:09:36 +0330

    feat(response macro): write a macro for generate responses

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,31 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // this macro generate all responses
+        Response::macro('gen', function (mixed $data, $meta = [], $errors = null, $code = 200) {
+            if ($errors != null) {
+                $errors = is_array($errors) && array_is_list($errors) ? $errors : [$errors]; // means if it's not key value array and it's an array of object 
+            }
+
+            if ($data instanceof AnonymousResourceCollection) {
+                $pagination = $data->resource->toArray(); // resource is the paginator
+                return Response::json([
+                    'data' => $data->collection,
+                    'meta' => [
+                        ...$meta,
+                        'current_page' => $pagination['current_page'] ?? null,
+                        'last_page' => $pagination['last_page'] ?? null,
+                        'per_page' => $pagination['per_page'] ?? null,
+                        'total' => $pagination['total'] ?? null,
+                    ],
+                    'errors' => $errors,
+                ], $code);
+            }
+            return Response::json([
+                'data' => $data,
+                'meta' => $meta,
+                'errors' => $errors
+            ], $code);
+        });
     }
 }
